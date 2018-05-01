a493f90e23a9f32ca4d4993ce62b03e5212104c7
VinayDhurwe <38860750+VinayDhurwe@users.noreply.github.com>
2018-05-01 19:38:24 +0530

    Scene-example-unity

---
+++ SceneExample/Assets/MenuScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour {
+
+	public Image beach, mountan, exit;
+
+	public void ClickedBeach()
+	{
+		SceneManager.LoadScene(1);
+	}
+
+	public void ClickMountain()
+	{
+		SceneManager.LoadScene(2);
+	}
+	public void ClickCity()
+	{
+		SceneManager.LoadScene(3);
+	}
+
+	public void ClickExit()
+	{
+		Application.Quit();
+	}
+
+	
+}
